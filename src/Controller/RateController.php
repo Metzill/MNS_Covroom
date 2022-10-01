43aa80ac9dbb627aa4bc6fd885281d756d98d997
Bastien <45488913+BastienBiblocque@users.noreply.github.com>
2022-10-01 17:21:14 +0200

    rate controller

--- src/Controller/RateController.php
+++ src/Controller/RateController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Rate;
+use App\Entity\User;
+use DateTime;
+use DateTimeZone;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,6 +22,36 @@ class RateController extends AbstractController
      */
     public function index(): Response
     {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/RateController.php',
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="new")
+     */
+    public function new(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $newPostJson = json_decode($request->getContent(), true);
+
+        $today = new DateTime();
+        $today->setTimezone(new DateTimeZone("UTC"));
+
+        $rate = new Rate();
+
+        $userFrom = $doctrine->getRepository(User::class)->find($newPostJson['idFrom']);
+        $userTo = $doctrine->getRepository(User::class)->find($newPostJson['idTo']);
+
+        $rate->setComment($newPostJson['comment']);
+        $rate->setRate($newPostJson['rate']);
+
+        $rate->setIdUserRating($userFrom);
+        $rate->setIdUserRated($userTo);
+
+        $rate->setCreatedAt($today);
+        $rate->setUpdatedAt($today);
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/RateController.php',
